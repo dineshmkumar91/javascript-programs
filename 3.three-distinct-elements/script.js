6cbbc91caf46a2a4ee18169b19ee8494f0ce4fa2
dineshmkumar91 <dineshmkumar91@gmail.com>
2024-11-24 23:15:09 +0530

    Passed test cases

--- 3.three-distinct-elements/script.js
+++ 3.three-distinct-elements/script.js
@@ -1,28 +1,39 @@
 
 function runFunction(){
-const arr = [12, 13, 90, 1, 10, 34, 1, 90];
+const arr = [8,8,2,2];
 let res = threeDistinctElements(arr);
 console.log(res);
 document.getElementById("output").innerHTML = res;
 }
 function threeDistinctElements(arr) {
     let first,second,third;
-    if(arr.length < 3){
-        return 'Invalid Input';
+     if(arr.length <= 3){
+        let sorted = arr.sort((a,b)=>b-a);
+        let newArr = [];
+        sorted.forEach(function(val){
+            if(!newArr.includes(val)){
+                newArr.push(val)
+            }
+        })
+     return newArr;
     }
-    third = first = second = Number.MIN_VALUE;
+    third = 0; first = 0; second =0;
     for (let i=0; i < arr.length; i++){
         if(arr[i] > first){
             third = second;
             second = first;
             first = arr[i];
-        }else if (arr[i] > second && first != arr[i]){
+        }else if (arr[i] > second && arr[i] < first){
             third = second;
             second = arr[i];
-        }else if(arr[i] > third && second != arr[i] && first != arr[i]){
+        }else if(arr[i] > third && arr[i] < second){
             third = arr[i];
         }
         }
-    return [first,second,third];
+    let res = [];
+    if(first){res.push(first)}
+    if(second){res.push(second)}
+    if(third){res.push(third)}
+    return res;
 
     }    
